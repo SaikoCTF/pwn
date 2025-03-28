3b18cd51ca6831af6d1d8bfb4c9b30b902398e8e
saiko-gamemaster <ctf-support@simspace.com>
2025-03-28 11:54:50 -0400

    update: scoreboard data for P66018 (2025-03-28T15:54:50.482Z)

--- public/leaderboard/assets/scripts/data.js
+++ public/leaderboard/assets/scripts/data.js
@@ -3,52 +3,52 @@ data = {
         {
     	   "id": 1,
              "name": "Ante Up",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 2,
              "name": "Where Did All The Malware Go?",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 3,
              "name": "War and Peas",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 4,
              "name": "Illegal Backward Pass",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 5,
              "name": "Fun and Games With User Accounts",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 6,
              "name": "It's All A Gamble",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 7,
              "name": "Who Has The Cure?",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 8,
              "name": "Peekaboo",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 9,
              "name": "Website Party Favors",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
         {
     	   "id": 10,
              "name": "Land of Fruits",
-             "allowed_time": ""
+             "allowed_time": "1:00:00.000000"
         },
 
     ],
@@ -73,6 +73,10 @@ data = {
     "id": "P66018",
     "handle": "FixedHarbinger",
     "challenges": [
+        {
+            "id": 3,
+            "time": "0:45:40.766584"
+        },
         {
             "id": 2,
             "time": "0:01:37.301057"
